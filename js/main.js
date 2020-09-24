f115ab1c0482fe4806298896b2613aa1cd9664d1
Robbie Andrews <robbie@bearpig.com>
2020-09-24 17:50:50 +0100

    Added 'positive cases identified by' and 'cases from green/amber/red countries'. Also added the 'Cases per 100k number'

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $.ajax({
             return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
         }
 
-        // Assign variables from JSON data
+// Assign variables from JSON data
         var date_val = data.COVID19[0].DateTime.replace("string;#", "");
         var confirmed_val = formatNumber(data.COVID19[0].Totalconfirmedpositivecases);
         var pending_val = formatNumber(data.COVID19[0].Pendingresults);
@@ -60,7 +60,18 @@ $.ajax({
         var reason_travel_val = formatNumber(data.COVID19[0].Reasonfortestinboundtravel);
         var reason_screening_val = formatNumber(data.COVID19[0].Reasonfortestonislandsurveillancescreening);
 
-        // 24 hour difference
+        var identified_symptomatic_val = formatNumber(data.COVID19[0].Positivecasesidentifiedsymptomaticindividualsseekinghealthcare);
+        var identified_air_val = formatNumber(data.COVID19[0].Positivecasesidentifiedinboundtravelbyair);
+        var identified_sea_val = formatNumber(data.COVID19[0].Positivecasesidentifiedinboundtravelbysea);
+        var identified_admissions_val = formatNumber(data.COVID19[0].Positivecasesidentifiedadmissionsscreening);
+        var identified_workforce_val = formatNumber(data.COVID19[0].Positivecasesidentifiedplannedworkforcescreening);
+        var identified_tracing_val = formatNumber(data.COVID19[0].Positivecasesidentifiedcontacttracing);
+
+        var country_green_val = formatNumber(data.COVID19[0].Positivecasesidentifiedinboundtravelfromgreencountries);
+        var country_red_val = formatNumber(data.COVID19[0].Positivecasesidentifiedinboundtravelfromredorambercountries);
+        var fortnight_val = data.COVID19[0].Fortnight14daynumberper100000;
+
+// 24 hour difference
         var total_tested_24_val = formatNumber((Number(data.COVID19[0].Totaltests) - Number(data.COVID19[1].Totaltests)).toFixed(0));
         var confirmed_24_val = (Number(data.COVID19[0].Totalconfirmedpositivecases) - Number(data.COVID19[1].Totalconfirmedpositivecases)).toFixed(0);
         var pending_24_val = (Number(data.COVID19[0].Pendingresults) - Number(data.COVID19[1].Pendingresults)).toFixed(0);
@@ -88,9 +99,18 @@ $.ajax({
         var reason_travel_24_val = formatNumber(Number(data.COVID19[0].Reasonfortestinboundtravel) - Number(data.COVID19[1].Reasonfortestinboundtravel));
         var reason_screening_24_val = formatNumber(Number(data.COVID19[0].Reasonfortestonislandsurveillancescreening) - Number(data.COVID19[1].Reasonfortestonislandsurveillancescreening));
        
-       
+        var identified_symptomatic_24_val = (Number(data.COVID19[0].Positivecasesidentifiedsymptomaticindividualsseekinghealthcare) - Number(data.COVID19[1].Positivecasesidentifiedsymptomaticindividualsseekinghealthcare));
+        var identified_air_24_val = (Number(data.COVID19[0].Positivecasesidentifiedinboundtravelbyair) - Number(data.COVID19[1].Positivecasesidentifiedinboundtravelbyair));
+        var identified_sea_24_val = (Number(data.COVID19[0].Positivecasesidentifiedinboundtravelbysea) - Number(data.COVID19[1].Positivecasesidentifiedinboundtravelbysea));
+        var identified_admissions_24_val = (Number(data.COVID19[0].Positivecasesidentifiedsymptomaticindividualsseekinghealthcare) - Number(data.COVID19[1].Positivecasesidentifiedsymptomaticindividualsseekinghealthcare));
+        var identified_workforce_24_val = (Number(data.COVID19[0].Positivecasesidentifiedplannedworkforcescreening) - Number(data.COVID19[1].Positivecasesidentifiedplannedworkforcescreening));
+        var identified_tracing_24_val = (Number(data.COVID19[0].Positivecasesidentifiedcontacttracing) - Number(data.COVID19[1].Positivecasesidentifiedcontacttracing));
 
-        // 7 day difference
+        var country_green_24_val = formatNumber(Number(data.COVID19[0].Positivecasesidentifiedinboundtravelfromgreencountries) - Number(data.COVID19[1].Positivecasesidentifiedinboundtravelfromgreencountries));
+        var country_red_24_val = formatNumber(Number(data.COVID19[0].Positivecasesidentifiedinboundtravelfromredorambercountries) - Number(data.COVID19[1].Positivecasesidentifiedinboundtravelfromredorambercountries));
+        var fortnight_24_val = (Number(data.COVID19[0].Fortnight14daynumberper100000) - Number(data.COVID19[1].Fortnight14daynumberper100000)).toFixed(0);
+
+// 7 day difference
         var total_tested_7_val = formatNumber((Number(data.COVID19[0].Totaltests) - Number(data.COVID19[6].Totaltests)).toFixed(0));
         var confirmed_7_val = (Number(data.COVID19[0].Totalconfirmedpositivecases) - Number(data.COVID19[6].Totalconfirmedpositivecases)).toFixed(0);
         var pending_7_val = (Number(data.COVID19[0].Pendingresults) - Number(data.COVID19[6].Pendingresults)).toFixed(0);
@@ -118,9 +138,19 @@ $.ajax({
         var reason_travel_7_val = formatNumber(Number(data.COVID19[0].Reasonfortestinboundtravel) - Number(data.COVID19[6].Reasonfortestinboundtravel));
         var reason_screening_7_val = formatNumber(Number(data.COVID19[0].Reasonfortestonislandsurveillancescreening) - Number(data.COVID19[6].Reasonfortestonislandsurveillancescreening));
 
+        var identified_symptomatic_7_val = (Number(data.COVID19[0].Positivecasesidentifiedsymptomaticindividualsseekinghealthcare) - Number(data.COVID19[6].Positivecasesidentifiedsymptomaticindividualsseekinghealthcare));
+        var identified_air_7_val = (Number(data.COVID19[0].Positivecasesidentifiedinboundtravelbyair) - Number(data.COVID19[6].Positivecasesidentifiedinboundtravelbyair));
+        var identified_sea_7_val = (Number(data.COVID19[0].Positivecasesidentifiedinboundtravelbysea) - Number(data.COVID19[6].Positivecasesidentifiedinboundtravelbysea));
+        var identified_admissions_7_val = (Number(data.COVID19[0].Positivecasesidentifiedsymptomaticindividualsseekinghealthcare) - Number(data.COVID19[6].Positivecasesidentifiedsymptomaticindividualsseekinghealthcare));
+        var identified_workforce_7_val = (Number(data.COVID19[0].Positivecasesidentifiedplannedworkforcescreening) - Number(data.COVID19[6].Positivecasesidentifiedplannedworkforcescreening));
+        var identified_tracing_7_val = (Number(data.COVID19[0].Positivecasesidentifiedcontacttracing) - Number(data.COVID19[6].Positivecasesidentifiedcontacttracing));
+
+        var country_green_7_val = formatNumber(Number(data.COVID19[0].Positivecasesidentifiedinboundtravelfromgreencountries) - Number(data.COVID19[6].Positivecasesidentifiedinboundtravelfromgreencountries));
+        var country_red_7_val = formatNumber(Number(data.COVID19[0].Positivecasesidentifiedinboundtravelfromredorambercountries) - Number(data.COVID19[6].Positivecasesidentifiedinboundtravelfromredorambercountries));
+        var fortnight_7_val = (Number(data.COVID19[0].Fortnight14daynumberper100000) - Number(data.COVID19[6].Fortnight14daynumberper100000)).toFixed(0);
      
 
-        // Add numbers from variables into HTML
+// Add numbers from variables into HTML
         $("#date").append(document.createTextNode(date_val));
         $("#confirmedcases").append(document.createTextNode(confirmed_val));
         $("#pendingresults").append(document.createTextNode(pending_val));
@@ -163,7 +193,18 @@ $.ajax({
         $("#reason_travel").append(document.createTextNode(reason_travel_val));
         $("#reason_screening").append(document.createTextNode(reason_screening_val));
 
+        $("#identified_symptomatic").append(document.createTextNode(identified_symptomatic_val));
+        $("#identified_air").append(document.createTextNode(identified_air_val));
+        $("#identified_sea").append(document.createTextNode(identified_sea_val));
+        $("#identified_admissions").append(document.createTextNode(identified_admissions_val));
+        $("#identified_workforce").append(document.createTextNode(identified_workforce_val));
+        $("#identified_tracing").append(document.createTextNode(identified_tracing_val));
 
+        $("#country_green").append(document.createTextNode(country_green_val));
+        $("#country_red").append(document.createTextNode(country_red_val));
+        $("#fortnight").append(document.createTextNode(fortnight_val));
+
+// 24 hours 
         $("#tested_24").append(document.createTextNode(total_tested_24_val));
         $("#confirmed_24").append(document.createTextNode(confirmed_24_val));
         $("#pending_24").append(document.createTextNode(pending_24_val));
@@ -183,8 +224,17 @@ $.ajax({
         $("#reason_symptomatic_24").append(document.createTextNode(reason_symptomatic_24_val));
         $("#reason_travel_24").append(document.createTextNode(reason_travel_24_val));
         $("#reason_screening_24").append(document.createTextNode(reason_screening_24_val));
+        $("#identified_symptomatic_24").append(document.createTextNode(identified_symptomatic_24_val));
+        $("#identified_air_24").append(document.createTextNode(identified_air_24_val));
+        $("#identified_sea_24").append(document.createTextNode(identified_sea_24_val));
+        $("#identified_admissions_24").append(document.createTextNode(identified_admissions_24_val));
+        $("#identified_workforce_24").append(document.createTextNode(identified_workforce_24_val));
+        $("#identified_tracing_24").append(document.createTextNode(identified_tracing_24_val));
+        $("#country_green_24").append(document.createTextNode(country_green_24_val));
+        $("#country_red_24").append(document.createTextNode(country_red_24_val));
+        $("#fortnight_24").append(document.createTextNode(fortnight_24_val));
      
-
+// 7 Days
         $("#tested_7").append(document.createTextNode(total_tested_7_val));
         $("#confirmed_7").append(document.createTextNode(confirmed_7_val));
         $("#pending_7").append(document.createTextNode(pending_7_val));
@@ -204,9 +254,17 @@ $.ajax({
         $("#reason_symptomatic_7").append(document.createTextNode(reason_symptomatic_7_val));
         $("#reason_travel_7").append(document.createTextNode(reason_travel_7_val));
         $("#reason_screening_7").append(document.createTextNode(reason_screening_7_val));
-     
+        $("#identified_symptomatic_7").append(document.createTextNode(identified_symptomatic_7_val));
+        $("#identified_air_7").append(document.createTextNode(identified_air_7_val));
+        $("#identified_sea_7").append(document.createTextNode(identified_sea_7_val));
+        $("#identified_admissions_7").append(document.createTextNode(identified_admissions_7_val));
+        $("#identified_workforce_7").append(document.createTextNode(identified_workforce_7_val));
+        $("#identified_tracing_7").append(document.createTextNode(identified_tracing_7_val));
+        $("#country_green_7").append(document.createTextNode(country_green_7_val));
+        $("#country_red_7").append(document.createTextNode(country_red_7_val));
+        $("#fortnight_7").append(document.createTextNode(fortnight_7_val));
        
-        // Set colour for data change value if positive/negative
+// Set colour for data change value if positive/negative
         total_tested_24_val < 0 ? $("#tested_24").addClass('negchange') : $("#tested_24").addClass('poschange');
         confirmed_24_val < 0 ? $("#confirmed_24").addClass('negchange') : $("#confirmed_24").addClass('poschange');
         pending_24_val < 0 ? $("#pending_24").addClass("negchange") : $("#pending_24").addClass("poschange");
@@ -226,8 +284,16 @@ $.ajax({
         reason_symptomatic_24_val < 0 ? $("#reason_symptomatic_24").addClass('negchange') : $("#reason_symptomatic_24").addClass('poschange');
         reason_travel_24_val < 0 ? $("#reason_travel_24").addClass('negchange') : $("#reason_travel_24").addClass('poschange');
         reason_screening_24_val < 0 ? $("#reason_screening_24").addClass('negchange') : $("#reason_screening_24").addClass('poschange');
+        identified_symptomatic_24_val < 0 ? $("#identified_symptomatic_24").addClass('negchange') : $("#identified_symptomatic_24").addClass('poschange');
+        identified_air_24_val < 0 ? $("#identified_air_24").addClass('negchange') : $("#identified_air_24").addClass('poschange');
+        identified_sea_24_val < 0 ? $("#identified_sea_24").addClass('negchange') : $("#identified_sea_24").addClass('poschange');
+        identified_admissions_24_val < 0 ? $("#identified_admissions_24").addClass('negchange') : $("#identified_admissions_24").addClass('poschange');
+        identified_workforce_24_val < 0 ? $("#identified_workforce_24").addClass('negchange') : $("#identified_workforce_24").addClass('poschange');
+        identified_tracing_24_val < 0 ? $("#identified_tracing_24").addClass('negchange') : $("#identified_tracing_24").addClass('poschange');
+        country_green_24_val < 0 ? $("#country_green_24").addClass('negchange') : $("#country_green_24").addClass('poschange');
+        country_red_24_val < 0 ? $("#country_red_24").addClass('negchange') : $("#country_red_24").addClass('poschange');
+        fortnight_24_val < 0 ? $("#fortnight_24").addClass('negchange') : $("#fortnight_24").addClass('poschange');
       
-
         total_tested_7_val < 0 ? $("#tested_7").addClass("negchange") : $("#tested_7").addClass("poschange");
         confirmed_7_val < 0 ? $("#confirmed_7").addClass("negchange") : $("#confirmed_7").addClass("poschange");
         pending_7_val < 0 ? $("#pending_7").addClass("negchange") : $("#pending_7").addClass("poschange");
@@ -247,6 +313,15 @@ $.ajax({
         reason_symptomatic_7_val < 0 ? $("#reason_symptomatic_7").addClass('negchange') : $("#reason_symptomatic_7").addClass('poschange');
         reason_travel_7_val < 0 ? $("#reason_travel_7").addClass('negchange') : $("#reason_travel_7").addClass('poschange');
         reason_screening_7_val < 0 ? $("#reason_screening_7").addClass('negchange') : $("#reason_screening_7").addClass('poschange');
+        identified_symptomatic_7_val < 0 ? $("#identified_symptomatic_7").addClass('negchange') : $("#identified_symptomatic_7").addClass('poschange');
+        identified_air_7_val < 0 ? $("#identified_air_7").addClass('negchange') : $("#identified_air_7").addClass('poschange');
+        identified_sea_7_val < 0 ? $("#identified_sea_7").addClass('negchange') : $("#identified_sea_7").addClass('poschange');
+        identified_admissions_7_val < 0 ? $("#identified_admissions_7").addClass('negchange') : $("#identified_admissions_7").addClass('poschange');
+        identified_workforce_7_val < 0 ? $("#identified_workforce_7").addClass('negchange') : $("#identified_workforce_7").addClass('poschange');
+        identified_tracing_7_val < 0 ? $("#identified_tracing_7").addClass('negchange') : $("#identified_tracing_7").addClass('poschange');
+        country_green_7_val < 0 ? $("#country_green_7").addClass('negchange') : $("#country_green_7").addClass('poschange');
+        country_red_7_val < 0 ? $("#country_red_7").addClass('negchange') : $("#country_red_7").addClass('poschange');
+        fortnight_7_val < 0 ? $("#fortnight_7").addClass('negchange') : $("#fortnight_7").addClass('poschange');
 
         if (total_tested_24_val == 0) { $("#tested_24").removeClass(["poschange", "negchange"])};
         if (confirmed_24_val == 0) { $("#confirmed_24").removeClass(["poschange", "negchange"])};
@@ -267,8 +342,16 @@ $.ajax({
         if (reason_symptomatic_24_val == 0) { $("#reason_symptomatic_24").removeClass(["poschange", "negchange"])};
         if (reason_travel_24_val == 0) { $("#reason_travel_24").removeClass(["poschange", "negchange"])};
         if (reason_screening_24_val == 0) { $("#reason_screening_24").removeClass(["poschange", "negchange"])};
-    
-
+        if (identified_symptomatic_24_val == 0) { $("#identified_symptomatic_24").removeClass(["poschange", "negchange"])};
+        if (identified_air_24_val == 0) { $("#identified_air_24").removeClass(["poschange", "negchange"])};
+        if (identified_sea_24_val == 0) { $("#identified_sea_24").removeClass(["poschange", "negchange"])};
+        if (identified_admissions_24_val == 0) { $("#identified_admissions_24").removeClass(["poschange", "negchange"])};
+        if (identified_workforce_24_val == 0) { $("#identified_workforce_24").removeClass(["poschange", "negchange"])};
+        if (identified_tracing_24_val == 0) { $("#identified_tracing_24").removeClass(["poschange", "negchange"])};
+        if (country_green_24_val == 0) { $("#country_green_24").removeClass(["poschange", "negchange"])};
+        if (country_red_24_val == 0) { $("#country_red_24").removeClass(["poschange", "negchange"])};
+        if (fortnight_24_val == 0) { $("#fortnight_24").removeClass(["poschange", "negchange"])};
+        
         if (total_tested_7_val == 0) { $("#tested_7").removeClass(["poschange", "negchange"])};
         if (confirmed_7_val == 0) { $("#confirmed_7").removeClass(["poschange", "negchange"])};
         if (pending_7_val == 0) { $("#pending_7").removeClass(["poschange", "negchange"])};
@@ -288,9 +371,16 @@ $.ajax({
         if (reason_symptomatic_7_val == 0) { $("#reason_symptomatic_7").removeClass(["poschange", "negchange"])};
         if (reason_travel_7_val == 0) { $("#reason_travel_").removeClass(["poschange", "negchange"])};
         if (reason_screening_7_val == 0) { $("#reason_screening_7").removeClass(["poschange", "negchange"])};
+        if (identified_symptomatic_7_val == 0) { $("#identified_symptomatic_7").removeClass(["poschange", "negchange"])};
+        if (identified_air_7_val == 0) { $("#identified_air_7").removeClass(["poschange", "negchange"])};
+        if (identified_sea_7_val == 0) { $("#identified_sea_7").removeClass(["poschange", "negchange"])};
+        if (identified_admissions_7_val == 0) { $("#identified_admissions_7").removeClass(["poschange", "negchange"])};
+        if (identified_workforce_7_val == 0) { $("#identified_workforce_7").removeClass(["poschange", "negchange"])};
+        if (identified_tracing_7_val == 0) { $("#identified_tracing_7").removeClass(["poschange", "negchange"])};
+        if (country_green_7_val == 0) { $("#country_green_7").removeClass(["poschange", "negchange"])};
+        if (country_red_7_val == 0) { $("#country_red_7").removeClass(["poschange", "negchange"])};
+        if (fortnight_7_val == 0) { $("#fortnight_7").removeClass(["poschange", "negchange"])};
      
-    
-
         $(".poschange").prepend("+");
 
         // Set widths for bar charts
